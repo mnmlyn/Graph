650fa02cebfcfb572eaeaca2aee3e4a936fba379
mnmlyn <mnmlyn@163.com>
2018-09-10 16:49:21 +0800

    实现图的表示，初始化函数，释放空间的函数

--- graph.cpp
+++ graph.cpp
@@ -1,8 +1,60 @@
 #include "graph.h"
 #include <iostream>
+#include <stdlib.h>//for malloc free
+
+#define NODE_NUM 6//图中节点数量
 
 int main()
 {
+    using namespace std;
 
+    
+
+    Graph *graph = GraphInit(NODE_NUM);
+    GraphDestroy(graph);
+
+
+    cout << "End\n";
+    cin.get();
     return 0;
+}
+
+//初始化邻接链表表示的图，初始化后为没有边相连的孤立节点图
+Graph* GraphInit(int n)
+{
+    VNodeptr G = (VNodeptr)malloc(sizeof(VNode)*n);
+    int i;
+    for(i=0;i<n;++i)
+    {
+        G->n = i;
+        G->color = WHITE;
+        G->d = INFINITE;
+        G->pi = NIL;
+        G->next = NULL;
+    }
+    Graph * graph = (Graph *)malloc(sizeof(Graph));
+    graph->G = G;
+    graph->nodeNum = n;
+}
+
+//释放邻接链表表示的图的存储空间
+void GraphDestroy(Graph *graph)
+{
+    //释放链表空间
+    int i,n=graph->nodeNum;
+    LNode *lnptr1,*lnptr2;
+    for(i=0;i<n;++i)
+    {
+        lnptr1 = graph->G->next;
+        while(lnptr1)
+        {
+            lnptr2 = lnptr1->next;
+            free(lnptr1);
+            lnptr1 = lnptr2;
+        }
+    }
+    //释放数组空间
+    free(graph->G);
+    //释放Graph结构体空间
+    free(graph);
 }
---
+++ graph.h
@@ -0,0 +1,38 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+#include <limits.h>//for INT_MAX
+
+//图的邻接链表表示法
+//邻接链表中的链表节点
+typedef struct LNode{
+    int n;
+    struct LNode *next;
+} LNode;
+
+//数组中的节点
+typedef struct VNode{
+    int n;
+    int color;
+    #define WHITE 0
+    #define GRAY 1
+    #define BLACK 2
+    int d;
+    #define INFINITE INT_MAX
+    int pi;
+    #define NIL -1
+    LNode *next;
+} VNode;
+//图指针，不包含节点数
+typedef VNode * VNodeptr;
+//图指针，包含节点数
+typedef struct Graph{
+    VNodeptr G;
+    int nodeNum;
+} Graph;
+
+//初始化邻接链表表示的图，初始化后为没有边相连的孤立节点图
+Graph* GraphInit(int n);
+//释放邻接链表表示的图的存储空间
+void GraphDestroy(Graph *graph);
+
+#endif
